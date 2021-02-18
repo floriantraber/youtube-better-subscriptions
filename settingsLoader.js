c24e4ad8b368ced71703b3166f6c7ed606d37077
Florian Traber <github@floriantraber.de>
2021-02-18 13:08:32 +0100

    Fix settings page doesn't load if storage is cleared.

--- settingsLoader.js
+++ settingsLoader.js
@@ -5,7 +5,9 @@ let settingsLoadedCallbacks = [];
 getSyncStorage().get(SETTINGS_KEY, items => {
     log("Settings loaded");
 
-    settings = {...settings, ...items[SETTINGS_KEY]};
+    if (items) {
+        settings = {...settings, ...items[SETTINGS_KEY]};
+    }
 
     for (let callback of settingsLoadedCallbacks) {
         callback();
